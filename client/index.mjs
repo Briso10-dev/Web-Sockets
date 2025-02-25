1867d0ff83431db7ee7fa762296d1160a715cb66
Briso10-dev <kouambrice10@gmail.com>
2025-02-25 06:22:23 +0100

    cors implemented

--- client/index.mjs
+++ client/index.mjs
@@ -1,4 +1,6 @@
+
 const socket = io("http://localhost:3000");
+
 
 socket.on("connect", (response)=>{
     console.log(response)
--- server/server.mjs
+++ server/server.mjs
@@ -2,15 +2,19 @@ import { createServer } from "http";
 import { Server } from "socket.io";
 import dotenv from 'dotenv'
 import chalk from "chalk";
+import cors from 'cors'
+
 dotenv.config()
+const corsOptions = {
+  origin: "http://127.0.0.1:5500",
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
 
 const httpServer = createServer();
-const io = new Server(httpServer, {
-  // ...
-});
+const socket = new Server(httpServer,cors(corsOptions));
 const port = process.env.PORT || 3000; // Default to 3000 if .env is missing
 
-io.on("connection", (socket) => {     //an event key connection implemented to the socket.io package    
+socket.on("connection", (socket) => {     //an event key connection implemented to the socket.io package    
   console.log(socket);
 });
 

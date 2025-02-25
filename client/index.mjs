b6f90abaacc68b09b09b185e73166e70d6d272c0
Briso10-dev <kouambrice10@gmail.com>
2025-02-25 06:33:09 +0100

    client-server connection established

--- client/index.mjs
+++ client/index.mjs
@@ -1,7 +1,10 @@
 
 const socket = io("http://localhost:3000");
 
-
 socket.on("connect", (response)=>{
     console.log(response)
 })
+
+socket.on('message',(data)=>{
+    console.log(data);
+});
--- server/server.mjs
+++ server/server.mjs
@@ -2,20 +2,21 @@ import { createServer } from "http";
 import { Server } from "socket.io";
 import dotenv from 'dotenv'
 import chalk from "chalk";
-import cors from 'cors'
 
 dotenv.config()
-const corsOptions = {
-  origin: "http://127.0.0.1:5500",
-  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-}
 
 const httpServer = createServer();
-const socket = new Server(httpServer,cors(corsOptions));
+const socket = new Server(httpServer,{
+  cors:{
+    origin: "http://127.0.0.1:5500",
+    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204    
+  }
+});
 const port = process.env.PORT || 3000; // Default to 3000 if .env is missing
 
 socket.on("connection", (socket) => {     //an event key connection implemented to the socket.io package    
   console.log(socket);
+  socket.emit('message','Hello')
 });
 
 httpServer.listen(port,()=>{
